035933244221415a024826730e97134e3e057d40
raghav <raghav@riktamtech.com>
2025-07-17 11:19:55 +0530

    removed helmet

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,4 @@
 import express from "express";
-import helmet from "helmet";
 import morgan from "morgan";
 import dotenv from "dotenv";
 
@@ -12,7 +11,6 @@ dotenv.config();
 
 const app = express();
 
-app.use(helmet());
 app.use(express.json({ limit: "10kb" }));
 app.use(morgan("dev"));
 
